ac276c545a5e1fd2863c91eecc625079e05c009e
shailuishai <148574174+shailuishai@users.noreply.github.com>
2025-01-20 20:11:51 +0300

    Update reviewDatabase.go

--- server/internal/modules/review/repo/database/reviewDatabase.go
+++ server/internal/modules/review/repo/database/reviewDatabase.go
@@ -52,11 +52,11 @@ func (db *ReviewDatabase) GetReview(reviewID uint) (*r.ReviewDTO, error) {
 	var userAvatarURL string
 	var filmPosterURL string
 
-	// Выполняем JOIN с таблицами users и film
+	// Выполняем JOIN с таблицами users и films
 	if err := db.db.Table("reviews").
-		Select("reviews.*, users.avatar_url as user_avatar_url, film.poster_url as film_poster_url").
+		Select("reviews.*, users.avatar_url as user_avatar_url, films.poster_url as film_poster_url").
 		Joins("JOIN users ON reviews.user_id = users.user_id").
-		Joins("JOIN film ON reviews.film_id = film.film_id").
+		Joins("JOIN films ON reviews.film_id = films.film_id").
 		Where("reviews.review_id = ?", reviewID).
 		First(&review).Scan(&userAvatarURL).Scan(&filmPosterURL).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
@@ -73,11 +73,11 @@ func (db *ReviewDatabase) GetReviewsByFilmID(filmID uint) ([]*r.ReviewDTO, error
 	var userAvatarURLs []string
 	var filmPosterURLs []string
 
-	// Выполняем JOIN с таблицами users и film
+	// Выполняем JOIN с таблицами users и films
 	if err := db.db.Table("reviews").
-		Select("reviews.*, users.avatar_url as user_avatar_url, film.poster_url as film_poster_url").
+		Select("reviews.*, users.avatar_url as user_avatar_url, films.poster_url as film_poster_url").
 		Joins("JOIN users ON reviews.user_id = users.user_id").
-		Joins("JOIN film ON reviews.film_id = film.film_id").
+		Joins("JOIN films ON reviews.film_id = films.film_id").
 		Where("reviews.film_id = ?", filmID).
 		Find(&reviews).Scan(&userAvatarURLs).Scan(&filmPosterURLs).Error; err != nil {
 		return nil, err
@@ -95,11 +95,11 @@ func (db *ReviewDatabase) GetReviewsByReviewerID(reviewerID uint) ([]*r.ReviewDT
 	var userAvatarURLs []string
 	var filmPosterURLs []string
 
-	// Выполняем JOIN с таблицами users и film
+	// Выполняем JOIN с таблицами users и films
 	if err := db.db.Table("reviews").
-		Select("reviews.*, users.avatar_url as user_avatar_url, film.poster_url as film_poster_url").
+		Select("reviews.*, users.avatar_url as user_avatar_url, films.poster_url as film_poster_url").
 		Joins("JOIN users ON reviews.user_id = users.user_id").
-		Joins("JOIN film ON reviews.film_id = film.film_id").
+		Joins("JOIN films ON reviews.film_id = films.film_id").
 		Where("reviews.user_id = ?", reviewerID).
 		Find(&reviews).Scan(&userAvatarURLs).Scan(&filmPosterURLs).Error; err != nil {
 		return nil, err
@@ -131,11 +131,11 @@ func (db *ReviewDatabase) GetAllReviews() ([]*r.ReviewDTO, error) {
 	var userAvatarURLs []string
 	var filmPosterURLs []string
 
-	// Выполняем JOIN с таблицами users и film
+	// Выполняем JOIN с таблицами users и films
 	if err := db.db.Table("reviews").
-		Select("reviews.*, users.avatar_url as user_avatar_url, film.poster_url as film_poster_url").
+		Select("reviews.*, users.avatar_url as user_avatar_url, films.poster_url as film_poster_url").
 		Joins("JOIN users ON reviews.user_id = users.user_id").
-		Joins("JOIN film ON reviews.film_id = film.film_id").
+		Joins("JOIN films ON reviews.film_id = films.film_id").
 		Find(&reviews).Scan(&userAvatarURLs).Scan(&filmPosterURLs).Error; err != nil {
 		return nil, err
 	}
