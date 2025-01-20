97d29a60a0c4dd2c3d68fd3fa72f23d285bc1bbb
shailuishai <148574174+shailuishai@users.noreply.github.com>
2025-01-21 01:38:55 +0300

    Update request.go

--- server/internal/modules/review/controller/request.go
+++ server/internal/modules/review/controller/request.go
@@ -1,7 +1,7 @@
 package controller
 
 type CreateReviewRequest struct {
-	UserID     uint   `json:"user_id" validate:"required"`
+	UserID     uint   `json:"user_id" validate:"omitempty"`
 	FilmID     uint   `json:"film_id" validate:"required"`
 	Rating     int    `json:"rating" validate:"required,min=0,max=100"`
 	ReviewText string `json:"review_text" validate:"required"`
